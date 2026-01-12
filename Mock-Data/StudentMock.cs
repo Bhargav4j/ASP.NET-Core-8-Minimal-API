3d9f54a0332822051733af9a4afb4d7f6c5e9f2c
Modernize Studio <studio@modernize.com>
2026-01-12 07:11:08 +0000

    Commit for planId: 104

--- Mock-Data/StudentMock.cs
+++ Mock-Data/StudentMock.cs
@@ -3,7 +3,7 @@ using MinimalAPIProject.Model;
 namespace MinimalAPIProject.Mock_Data;
 public class StudentMock
 {
-    public static IEnumerable<Student> students = new List<Student>(){
+    public static List<Student> students = new List<Student>(){
         new Student() { Name = "Test 1", Age = new Random().Next(18, 60), Id = Guid.NewGuid() },
         new Student() { Name = "Test 2", Age = new Random().Next(18, 60), Id = Guid.NewGuid() },
         new Student() { Name = "Test 3", Age = new Random().Next(18, 60), Id = Guid.NewGuid() },
--- Repository/StudentRepository.cs
+++ Repository/StudentRepository.cs
@@ -7,7 +7,7 @@ public class StudentRepository : IStudentRepository
 
     public async Task<Student> CreateStudent(Student student)
     {
-        StudentMock.students = StudentMock.students.Append(student);
+        StudentMock.students.Add(student);
         return await Task.Run(() => student);
     }
 
@@ -15,8 +15,8 @@ public class StudentRepository : IStudentRepository
     {
         Student? student = StudentMock.students.FirstOrDefault(x => x.Id == id);
         if(student is null) return await Task.Run(() => false);
-        StudentMock.students = StudentMock.students.Where(x => x.Id != id);
-        return await Task.Run(() => true); 
+        StudentMock.students.Remove(student);
+        return await Task.Run(() => true);
     }
 
     public async Task<IEnumerable<Student>> GetAllStudents()
@@ -33,12 +33,8 @@ public class StudentRepository : IStudentRepository
     {
         Student? student_ = StudentMock.students.FirstOrDefault(x => x.Id == student.Id);
         if(student_ is null) return null;
-        student_ = student;
-        StudentMock.students = StudentMock.students.Select(stu => {
-            if(stu.Id == student_.Id)
-                return student_;
-            else return stu;
-        });
+        int index = StudentMock.students.IndexOf(student_);
+        StudentMock.students[index] = student;
         return await Task.Run(() => true);
     }
 }
